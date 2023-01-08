6cc1a98252f9a22c2184b37346b03458906dce36
ishika_sethi <ishika.sethi@wisdmlabs.com>
2023-01-09 05:22:31 +0530

    bug fixes

--- send_email.py
+++ send_email.py
@@ -6,12 +6,12 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "ishikasethi2806@gmail.com"
-    password = os.getenv("PASSWORD")
+    password = "qrkuzdzetfkknzgt"
     receiver = "ishikasethi2806@gmail.com"
     context = ssl.create_default_context()
 
-    print(username)
-    print(password)
+    # print(username)
+    # print(password)
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
